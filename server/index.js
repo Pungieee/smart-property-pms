3294fd88d8f2e4b9fb7b206e8fa25952eac9affe
Pungieee <keanharlz@gmail.com>
2025-12-18 01:40:07 +0700

    final update

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const rawData = require('./server/data.json');
+const rawData = require('./data.json');
 
 app.use(cors());
 app.use(express.json());
